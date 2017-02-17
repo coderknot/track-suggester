50cf4247db85f559341fbda381aa33b1a52a01ec
CoderKnot <coder.knot@gmail.com>
2017-02-17 11:29:37 -0800

    add three score factor variables and logic to calculate each score factor based on form inputs in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -8,5 +8,43 @@ $(document).ready(function() {
     var enterprise = parseInt($("#enterprise").val());
     var microsoft = $("input:radio[name='microsoft']:checked").val();
 
+    var designScore = 0;
+    var programmingScore = 0;
+    var enterpriseScore = 0;
+
+    if(design === "yes") {
+      designScore += 6;
+    } else {
+      programmingScore += 1;
+    }
+
+    if(ruby === "yes") {
+      programmingScore += 6;
+    } else {
+      enterpriseScore += 1;
+    }
+
+    if(php === 3) {
+      programmingScore += 3;
+    } else if (php === 1) {
+      designScore += 1;
+    } else {
+      enterpriseScore += 1;
+    }
+
+    if(enterprise === 3) {
+      enterpriseScore += 2;
+    } else if (enterprise === 1) {
+      designScore += 1;
+    } else {
+      programmingScore += 1;
+    }
+
+    if(microsoft === "yes") {
+      enterpriseScore += 6;
+    } else {
+      enterpriseScore += 0;
+    }
+
   });
 });
